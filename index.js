a4c8df12c3dbc1334d41f6184b13b298645472cf
Joao Esperancinha <jofisaes@gmail.com>
2022-05-07 20:44:31 +0200

    Unzips Java Loom

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const http = require('https'); // or 'https' for https:// URLs
 const fs = require('fs');
+const tar = require('tar-fs');
+const zlib = require('zlib');
 
-loom="19-loom+6-625"
+loom = "19-loom+6-625"
 const file = fs.createWriteStream("openjdk-19.tar.gz");
 const request = http.get("https://download.java.net/java/early_access/loom/6/openjdk-" + loom + "_linux-x64_bin.tar.gz", function (response) {
     response.pipe(file);
     file.on("finish", () => {
         file.close();
         console.log("Download Completed");
-
+        fs.createReadStream("openjdk-19.tar.gz")
+            .pipe(zlib.createGunzip())
+            .pipe(tar.extract("loom-jdk"))
+            .on("finish", () => {
+                console.log("Unzipped JDK Loom");
+            });
     });
 });
 
